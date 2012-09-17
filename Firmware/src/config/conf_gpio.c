8a4db4288cb6dc040a0a1c0ec24a8cbfbb0a12b1
Pekka Savolainen <savolainen.pekka@gmail.com>
2012-09-17 21:00:49 +0300

    Refactored, still working.

--- Firmware/src/config/conf_gpio.c
+++ Firmware/src/config/conf_gpio.c
@@ -4,31 +4,27 @@
 const conf_gpio_t conf_gpio_settings[] =
 {
 	{
-		.pin = PIN_USART_TXD,						// PIN
-		.function = AVR32_USART0_TXD_0_FUNCTION,		// FUNCTION
-		.channel = CHANNEL_NOT_DEFINED,				// CHANNEL	
-		.mode = INIT_AS_GPIO,						// MODE
+		.pin = PIN_USART_TXD,
+		.function = AVR32_USART0_TXD_0_FUNCTION,
+		.channel = CHANNEL_NOT_DEFINED,	
+		.mode = INIT_AS_GPIO,		
 		.is_last = SC_FALSE,
 	},
 	{
-		.pin = PIN_USART_RXD,						// PIN
-		.function = AVR32_USART0_RXD_0_FUNCTION,		// FUNCTION
-		.channel = CHANNEL_NOT_DEFINED,				// CHANNEL
-		.mode = INIT_AS_GPIO,						// MODE
+		.pin = PIN_USART_RXD,		
+		.function = AVR32_USART0_RXD_0_FUNCTION,	
+		.channel = CHANNEL_NOT_DEFINED,			
+		.mode = INIT_AS_GPIO,						
 		.is_last = SC_FALSE,
 	},
 	{
-		AVR32_PWMA_28_PIN,					// PIN
-		AVR32_PWMA_28_FUNCTION,				// FUNCTION
-		28,									// CHANNEL
-		INIT_AS_PWM,						// MODE
-		SC_FALSE,
+		.pin = AVR32_PWMA_28_PIN,
+		.function = AVR32_PWMA_28_FUNCTION,		
+		.channel = 28,			
+		.mode = INIT_AS_PWM,						
+		.is_last = SC_FALSE,
 	},
 	{
-		END_OF_ARRAY,
-		END_OF_ARRAY,
-		END_OF_ARRAY,
-		INIT_AS_GPIO,
-		SC_TRUE
+		.is_last = SC_TRUE
 	}
 };
